1ff6251f07b97ac66550276781ec558eef018457
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2014-03-06 14:24:08 +0000

    nothing

--- js/sevianno-draggable.js
+++ js/sevianno-draggable.js
@@ -2,23 +2,25 @@
 var init_dragable = function(){
     if(typeof $("#q").draggable !== 'function') 
             gadgets.util.registerOnLoadHandler(init_dragable);
-    $("#q").draggable({
-        axis: "y",
-        start: function(){
-            var $c = $("#canvas-frame");
-            $c.css('bottom', 'inherit');
-            $(this).css('height',50);
-        },
-        drag: function( event, ui ) {
-            var height = ui.position.top-30;
-            $("#canvas-frame").css('height', height);
-            gadgets.window.adjustHeight();
-        },
-        stop: function(){
-            $(this).css('height',3);
-            gadgets.window.adjustHeight();
-            $(this).css('top','');
-        }
-    });
+    else { 
+        $("#q").draggable({
+           axis: "y",
+            start: function(){
+                var $c = $("#canvas-frame");
+                $c.css('bottom', 'inherit');
+                $(this).css('height',50);
+            },
+            drag: function( event, ui ) {
+                var height = ui.position.top-30;
+                $("#canvas-frame").css('height', height);
+                gadgets.window.adjustHeight();
+            },
+            stop: function(){
+                $(this).css('height',3);
+                gadgets.window.adjustHeight();
+                $(this).css('top','');
+            }
+        });
+    }
 }
 gadgets.util.registerOnLoadHandler(init_dragable);
