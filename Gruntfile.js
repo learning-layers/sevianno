46528d145f9b701ff4f2d2a5d53c22cc0de0a269
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2014-03-10 16:28:17 +0000

    fixed githooks

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,8 +30,8 @@ module.exports = function(grunt) {
     githooks: {
       all: {
         // Will run the jshint and test:unit tasks at every commit
-        'pre-commit': 'replace:main replace:js replace:css',
-        'post-commit': 'replace:maindev replace:jsdev replace:cssdev'
+        'pre-commit': 'replaceUrlsProduction',
+        'post-commit': 'replaceUrlsDevelopment'
       }
     },
     watch: {
@@ -102,8 +102,8 @@ module.exports = function(grunt) {
           from: 'https://raw.github.com/DadaMonad/sevianno/master/widgets/',                   // string replacement
           to: 'http://54.229.235.99:1337/'
         }]      
-      }
-      /*readme: {
+      },
+      readme: {
         src: ['README.md'],       // source files array (supports minimatch)
         dest: 'README.md',             // destination directory or file
         replacements: [{
@@ -118,7 +118,7 @@ module.exports = function(grunt) {
           from: 'https://raw.github.com/DadaMonad/sevianno/master/widgets/',                   // string replacement
           to: 'http://54.229.235.99:1337/'
         }]      
-      }*/
+      }
     }
   });
 
@@ -131,10 +131,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-githooks');
 
   // Default task.
-  grunt.registerTask('replaceUrlsProduction', ['replace:main', 'replace:js', 'replace:css']);
-  grunt.registerTask('replaceUrlsDevelopment', ['replace:maindev', 'replace:jsdev', 'replace:cssdev']);
+  grunt.registerTask('replaceUrlsProduction', ['replace:main', 'replace:js', 'replace:css', 'replace:readme']);
+  grunt.registerTask('replaceUrlsDevelopment', ['replace:maindev', 'replace:jsdev', 'replace:cssdev', 'replace:readmedev']);
   grunt.registerTask('servewidgets', ['connect']);
-  grunt.registerTask('githooks', ['githooks']);
+  grunt.registerTask('save-githooks', ['githooks']);
   grunt.registerTask('default', ['githooks']);
 
 
